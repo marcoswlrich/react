a7617a2bf42a621b9b620b59e8b91bdedf7faddb
Marcos Wlrich <marcosapw.dev@gmail.com>
2022-01-26 16:26:49 -0300

    Correções de bug

--- listatarefas/src/App.js
+++ listatarefas/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./App.css";
+import React from 'react'
+import './App.css'
 
 export default function App() {
-  return <h1> Hello Word</h1>;
+  return <h1> Hello Word</h1>
 }
